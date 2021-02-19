8ef26f0b36c18d2cf39c46f78a6f75c001f79022
Anoop Varghese <avarghese@atlassian.com>
2021-02-19 22:59:51 +1100

    remove user data

---
+++ azure/settings.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "writing env"
+mkdir /home/ansible-files
+echo "{\"hostname\": \"${hostname}\",\"rcon_password\": \"${rcon_password}\",\"sv_password\": \"${sv_password}\",\"maxplayers\": \"32\",\"tickrate\": \"128\",\"host_info\": \"2\",\"gslt\": \"${gslt}\", \"tags\": \"csgo,executes,mirage\",\"mapgroup\": \"de_mirage\",\"defaultmap\": \"de_mirage\"}" > /home/ansible-files/env.json
+
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+
+sudo bash -c 'echo "localhost" > /etc/ansible/hosts'
+
+
