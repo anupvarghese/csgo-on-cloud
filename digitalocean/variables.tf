443a11a23f814ebf73f68fb8644fdde9f6fe6dd3
Anoop Varghese <avarghese@atlassian.com>
2021-06-14 09:29:43 +1000

    fix vars

--- digitalocean/variables.tf
+++ digitalocean/variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   default = "sgp1"
 }
 
-variable "resource_group_name" {
-  default = "TERRAFORM-RG03"
-}
-
 variable "digitalocean_api_key" {
   default = "123"
 }
@@ -20,28 +16,9 @@ variable "private_key_path" {
   default     = "~/.ssh/id_rsa"
 }
 
-variable "vpc" {
-  description = "Azure Network"
-  default     = "10.0.0.0/16"
-}
-
-variable "subnet" {
-  description = "Azure Subnet"
-  default     = "10.0.2.0/24"
-}
-
 variable "vm_size" {
-  description = "Azure VM Size"
-  default     = "Standard_DS1_v2"
-}
-
-variable "storage_disk_type" {
-  description = "Managed disk type"
-  default     = "Standard_F2"
-}
-variable "environment_tag" {
-  description = "Environment tag"
-  default     = "Staging"
+  description = "Digital Ocean VM Size"
+  default     = "s-1vcpu-1gb"
 }
 
 variable "sv_password" {
